4320f903286b3d7ad7ee8fd44111c76587513b1b
dpaschal-lf <daniel.paschal@learningfuze.com>
2019-04-16 12:46:25 -0700

    updated for es6

--- genericFirebaseBackend.js
+++ genericFirebaseBackend.js
@@ -1,37 +1,40 @@
-	function GenericFBModel(gameName, changeCallbackFunction){
-		this.boardName = gameName;
-		this.db;
-		this.callback = changeCallbackFunction;
-		this.lastSend = null;
-		this.initialize = function(){
+	class GenericFBModel{
+		constructor(gameName, changeCallbackFunction){
+			this.boardName = gameName;
+			this.db = null;
+			this.callback = changeCallbackFunction;
+			this.lastSend = null;
+			
+			this.start = this.start.bind( this );
+			this.initialize();
+		}
+		initialize(){
 			this.load();
 		}
-		this.load = function(){
-			$.getScript('https://www.gstatic.com/firebasejs/3.6.8/firebase.js',this.start.bind(this));
+		load(){
+			$.getScript('https://www.gstatic.com/firebasejs/3.6.8/firebase.js',this.start);
 		}
-		this.start = function(){
+		start(){
 		  	this.db=firebase;
 			this.db.initializeApp(firebaseConfig);
 			this.registerListener();
 		}
-		this.saveState = function(newState){
+		saveState(newState){
 			this.lastSend = JSON.stringify(newState);
 			this.db.database().ref(this.boardName).set(newState);
 		}
-		this.getAllData = function( dataCallback ){
+		getAllData( dataCallback ){
 			this.db.database().ref(this.boardName).once('value', (snapShot)=>{
 				dataCallback( snapshot.val() );
 			});
 		}
-		this.registerListener = function(){
+		registerListener(){
 			this.db.database().ref(this.boardName).on('value',this.handleDataUpdate.bind(this));
 		}
-		this.handleDataUpdate = function(data){
+		handleDataUpdate(data){
 			var currentData = JSON.stringify(data.val());
 			if(currentData!=this.lastSend){
 				this.callback.call(null,data.val());
 			}
 		}
-		this.initialize();
-
 	}
